2079b6d506219cfd21ea92930508386c34b79cd9
Tim Strazzere <diff@sentinelone.com>
2017-04-07 12:27:41 -0700

    Cleaning up code and added min version for compilation

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc shim.c -o nophos
+	gcc nophos.c -o nophos -mmacosx-version-min=10.11.5
--- nophos.c
+++ nophos.c
@@ -13,7 +13,6 @@
 #define OAS_SET_LOG_LEVEL          4
 #define OAS_SET_DIAGNOSTICS_ENABLE 5
 
-
 #define OAS_GET_KEXT_SCHEMA_LEVEL  1
 #define OAS_GET_KEXT_LOG_LEVEL     2
 
@@ -21,61 +20,68 @@
 #define SWI_GET_REDIRECT_PORT      1
 #define SWI_GET_KEXT_LOG_LEVEL     2
 #define SWI_GET_KEXT_SCHEMA_LEVEL  3
-#define SWI_GET_KEXT_UNKNOWN  4
+#define SWI_GET_KEXT_UNKNOWN       4
 
 int main() {
+  int result = -1;
+  struct sockaddr_ctl addr;
+
   printf("Sophos fun...\n");
 
+  // Create a socket
   int fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
-
-  struct sockaddr_ctl       addr;
-  int                       ret = 1;
- 
   if (fd != -1) {
-    bzero(&addr, sizeof(addr)); // sets the sc_unit field to 0
+    // Initialize the address structure
+    bzero(&addr, sizeof(addr));
     addr.sc_len = sizeof(addr);
     addr.sc_family = AF_SYSTEM;
     addr.ss_sysaddr = AF_SYS_CONTROL;
 
+    // create and initialize the info structure
     struct ctl_info info;
-    memset(&info, 0, sizeof(info));
+    bzero(&info, sizeof(info));
+    
     // Web sockets : com.sophos.nke.swi
     // OaS: com.sophos.kext.oas
     //strncpy(info.ctl_name, "com.sophos.kext.oas", sizeof(info.ctl_name));
     strncpy(info.ctl_name, "com.sophos.nke.swi", sizeof(info.ctl_name));
-    if (ioctl(fd, CTLIOCGINFO, &info)) {
-      printf("Could not get ID for kernel control.\n");
-      return -1; // exit(-1);
-    }
-
-    addr.sc_id = info.ctl_id;
-    addr.sc_unit = 0;
-
-    int result = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+    result = ioctl(fd, CTLIOCGINFO, &info);
     if (result) {
-      fprintf(stderr, "connect failed %d\n", result);
-    }
+      printf("Could not get ID for kernel control.\n");
+    } else {
 
-    printf("result = 0x%02x\n", result);
+      // Set address socket id
+      addr.sc_id = info.ctl_id;
+      addr.sc_unit = 0;
 
-    int kEPCommand1 = 2;
-
-
-    // getsockopt swi command 3 for getting expected schema : returns 6
-    // getsockopt oas command 1 for getting expected schema : returns 7
-    
-    if(!result) {
-      size_t o;
-      socklen_t len = sizeof(o);
-      result = getsockopt(fd, SO_ACCEPTCONN, 1, &o, &len);
-	    //result = getsockopt(fd, SO_ACCEPTCONN, 3, &o, &len);
-      if (result){
-        fprintf(stderr, "getsockopt failed on cmd call - result was %d\n", result);
+      // Connect to the socket
+      result = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+      if (result) {
+	fprintf(stderr, "connect failed %d\n", result);
       } else {
-	printf("getsockopt success : 0x%02x\n", o);
+	printf("result = 0x%02x\n", result);
+	// getsockopt swi command 3 for getting expected schema : returns 6
+	// getsockopt oas command 1 for getting expected schema : returns 7
+	size_t o;
+	bzero(&o, sizeof(o));
+	socklen_t len = sizeof(o);
+	result = getsockopt(fd, SO_ACCEPTCONN, 3, &o, &len);
+	if (result){
+	  fprintf(stderr, "getsockopt failed on cmd call - result was %d\n", result);
+	} else {
+	  printf("getsockopt success : 0x%02x\n", o);
+	}
       }
     }
-    
+    printf("Closing socket...\n");
+    if(close(fd)) {
+      fprintf(stderr, "close failed\n");
+    }
+  }
+  printf("Done...\n");
+}
+
+    /*
     if (!result) {
       char *secret = "3";
       socklen_t len = sizeof(secret);
@@ -83,16 +89,8 @@ int main() {
       if (result){
 	fprintf(stderr, "setsockopt failed on cmd call - result was %d\n", result);
       } else {
-	printf("getsockopt success : 0x%02x\n", result);
+	printf("setsockopt success : 0x%02x\n", result);
       }
     }
+    */
 
-  }
-
-  printf("Closing socket...\n");
-  if(close(fd)) {
-    fprintf(stderr, "close failed\n");
-  }
-  
-  printf("Done...\n");
-}
